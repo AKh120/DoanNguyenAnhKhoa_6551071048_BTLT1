a349b803d9243c098269391456bf81dbe4833c0e
AKh120 <6551071048@st.utc2.edu.vn>
2026-08-24 12:38:52 +0700

    Add files via upload

---
+++ Cau3/ChuyenDoiNhietDo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ChuyenDoiNhietDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Khai bao bien
+            decimal celsius;
+            decimal fahrenheit;
+            decimal kelvin;
+
+            // Nhap nhiet do Celsius
+            Console.Write("Nhap nhiet do Celsius: ");
+            celsius = decimal.Parse(Console.ReadLine());
+
+            // Chuyen doi sang Fahrenheit
+            fahrenheit = celsius * 9 / 5 + 32;
+
+            // Chuyen doi sang Kelvin
+            kelvin = celsius + 273.15m;
+
+            // In ket qua voi 2 chu so thap phan
+            Console.WriteLine();
+            Console.WriteLine("Nhiet do Fahrenheit: {0:F2}", fahrenheit);
+            Console.WriteLine("Nhiet do Kelvin: {0:F2}", kelvin);
+        }
+    }
+}
