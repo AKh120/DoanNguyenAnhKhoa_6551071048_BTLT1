86738c91849d06895236645d1296432b46e1345d
AKh120 <6551071048@st.utc2.edu.vn>
2026-08-24 12:46:59 +0700

    Add files via upload

---
+++ Cau5/XepLoaiHocSinh/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace XepLoaiHocSinh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Khai bao bien
+            double diem;
+
+            // Nhap diem
+            Console.Write("Nhap diem trung binh: ");
+            diem = double.Parse(Console.ReadLine());
+
+            // Kiem tra va xep loai
+            if (diem < 0 || diem > 10)
+            {
+                Console.WriteLine("Diem khong hop le!");
+            }
+            else if (diem >= 9.0)
+            {
+                Console.WriteLine("Xep loai: Xuat sac");
+            }
+            else if (diem >= 8.0)
+            {
+                Console.WriteLine("Xep loai: Gioi");
+            }
+            else if (diem >= 6.5)
+            {
+                Console.WriteLine("Xep loai: Kha");
+            }
+            else if (diem >= 5.0)
+            {
+                Console.WriteLine("Xep loai: Trung binh");
+            }
+            else
+            {
+                Console.WriteLine("Xep loai: Yeu");
+            }
+        }
+    }
+}
